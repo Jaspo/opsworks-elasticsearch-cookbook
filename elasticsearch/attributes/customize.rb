6e362f9dfae5295c4e5b8987bb77eb9d9d864cd4
Claudio Umaña Valverde <claudiouv@gmail.com>
2016-03-18 01:03:02 +0100

    Update customize.rb

--- elasticsearch/attributes/customize.rb
+++ elasticsearch/attributes/customize.rb
@@ -1,3 +1,2 @@
 normal[:elasticsearch][:version] = '2.2.0'
-normal[:elasticsearch][:plugins]['elasticsearch/elasticsearch-cloud-aws'][:version] = '2.7.1' # Must correpond to the proper Elasticsearch version. See the elasticsearch-cloud-aws repo for more info.
 normal[:elasticsearch][:cluster][:name] = 'best_cluster'  # Change to whatever you want.
