883216df3304901472394507d69f691d384bf81d
Claudio Umaña Valverde <claudiouv@gmail.com>
2016-03-18 12:12:31 +0100

    Update customize.rb

--- elasticsearch/attributes/customize.rb
+++ elasticsearch/attributes/customize.rb
@@ -1,3 +1,3 @@
 normal[:elasticsearch][:version] = '2.2.0'
-default['elasticsearch']['plugins'] = {discovery-ec2} # Must correpond to the proper Elasticsearch version. See the elasticsearch-cloud-aws repo for more info.
+default['elasticsearch']['plugins'] = {'discovery-ec2'} # Must correpond to the proper Elasticsearch version. See the elasticsearch-cloud-aws repo for more info.
 normal[:elasticsearch][:cluster][:name] = 'best_cluster'  # Change to whatever you want.
